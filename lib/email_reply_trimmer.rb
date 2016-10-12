1c291d01f6cf64194b97dd669a739e094965ae5e
Régis Hanol <regis@hanol.fr>
2016-10-12 19:44:37 +0200

    bump version

--- lib/email_reply_trimmer.rb
+++ lib/email_reply_trimmer.rb
@@ -6,7 +6,7 @@ require_relative "email_reply_trimmer/email_header_matcher"
 require_relative "email_reply_trimmer/quote_matcher"
 
 class EmailReplyTrimmer
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 
   DELIMITER    = "d"
   EMBEDDED     = "b"
