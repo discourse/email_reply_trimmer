7be0610f1e709be0792bdc27d9074d2e8eab4765
Gerhard Schlager <mail@gerhard-schlager.at>
2017-09-14 16:47:54 +0200

    bump version

--- lib/email_reply_trimmer.rb
+++ lib/email_reply_trimmer.rb
@@ -6,7 +6,7 @@ require_relative "email_reply_trimmer/email_header_matcher"
 require_relative "email_reply_trimmer/quote_matcher"
 
 class EmailReplyTrimmer
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 
   DELIMITER    = "d"
   EMBEDDED     = "b"
