fd1f2e82397226391c1d12c0d7fdf69a425e2385
Bianca Nenciu <nenciu.bianca@gmail.com>
2020-06-04 18:54:58 +0300

    Bump version

--- lib/email_reply_trimmer.rb
+++ lib/email_reply_trimmer.rb
@@ -6,7 +6,7 @@ require_relative "email_reply_trimmer/email_header_matcher"
 require_relative "email_reply_trimmer/quote_matcher"
 
 class EmailReplyTrimmer
-  VERSION = "0.1.12"
+  VERSION = "0.1.13"
 
   DELIMITER    = "d"
   EMBEDDED     = "b"
